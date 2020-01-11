142c1fbfa7f54eb3900d687836d95c12a2e2b41c
KanishAnand <therealkanish@gmail.com>
2020-01-11 15:37:50 +0530

    gravity part in process

--- t.py
+++ t.py
@@ -4,7 +4,6 @@ from select import select
 import termios
 import subprocess as sp
 # import atexit
-import time
 import tty
 import sys
 import os
@@ -13,26 +12,6 @@ colorama.init()
 rows, columns = os.popen('stty size', 'r').read().split()
 world_x = int(columns)
 world_y = int(rows) - 1
-
-
-def kbhit():
-    dr, dw, de = select([sys.stdin], [], [], 0)
-    return dr != []
-
-
-def getch():
-    return sys.stdin.read(1)[0]
-
-
-def hide_cursor():
-    sys.stdout.write("\033[?25l")
-    sys.stdout.flush()
-
-
-def show_cursor():
-    sys.stdout.write("\033[?25h")
-    sys.stdout.flush()
-
 
 class Person:
     def __init__(self):
@@ -51,9 +30,6 @@ class Person:
         if self.pos_y < 1:
             self.pos_y = 1
 
-        print(Back.BLUE)
-        sp.call('clear', shell=True)
-        # print(colorama.ansi.clear_screen())
         print(Fore.RED + "\033["+str(self.pos_y) +
               ";"+str(self.pos_x)+"H"+self.char)
 
@@ -68,11 +44,29 @@ class Person:
         elif(val == 'w' or val == 'W'):
             self.pos_y -= 1
 
-
 if __name__ == "__main__":
+    clear()
     # disabling buffering so you don't have to press enter
     orig_settings = termios.tcgetattr(sys.stdin)
     tty.setcbreak(sys.stdin)
+
+    while(1):
+        print(Back.BLUE)
+        print(colorama.ansi.clear_screen())
+        print_char(pos_x, pos_y, "+")
+        val = sys.stdin.read(1)[0]
+        move(val)
+
+    # enabling previous settings
+    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, orig_settings)
+
+
+
+if __name__ == "__main__":
+    # disabling buffering so you don't have to press enter
+    # atexit.register(set_orig_settings)
+    orig_settings = termios.tcgetattr(sys.stdin)
+    set_new_settings()
     player = Person()
     hide_cursor()
 
@@ -86,7 +80,9 @@ if __name__ == "__main__":
             # use gravity when nothing is pressed
             player.gravity()
         player.update()
-        # time.sleep(0.5)
 
     show_cursor()
-    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, orig_settings)
+    set_orig_settings()
+
+    # enabling previous settings
+    # termios.tcsetattr(sys.stdin, termios.TCSAFLUSH, orig_settings)
